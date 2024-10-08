f2cd97b486567d24058cd8281a273bfe8fdc45a9
loosh <notloosh@gmail.com>
2024-10-07 18:49:48 -0700

    Add sign in redirect URL for Clerk sign in component

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -35,9 +35,17 @@ export default function Header() {
           <DashboardButton />
         </SignedIn>
         <SignedOut>
-          <HeaderButton>
-            <SignInButton />
-          </HeaderButton>
+          <div className='group relative inline-flex text-nowrap h-10 min-w-24 items-center justify-center overflow-hidden rounded-full border border-neutral-200 bg-white text-sm font-medium'>
+            <div className='inline-flex h-12 translate-y-0 items-center justify-center px-6 text-neutral-950 transition duration-500 group-hover:-translate-y-[150%]'>
+              <SignInButton forceRedirectUrl={'/editor'} />
+            </div>
+            <div className='absolute inline-flex h-12 w-full translate-y-[100%] items-center justify-center text-neutral-50 transition duration-500 group-hover:translate-y-0'>
+              <span className='absolute h-full w-full translate-y-full skew-y-12 scale-y-0 bg-blue-500 transition duration-500 group-hover:translate-y-0 group-hover:scale-150'></span>
+              <span className='z-10'>
+                <SignInButton forceRedirectUrl={'/editor'} />
+              </span>
+            </div>
+          </div>
         </SignedOut>
         <PopoverMenu />
       </div>

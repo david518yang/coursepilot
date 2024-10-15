9c59d2dca04ea95c612b16f8a4183c9fa8590f8d
David Yang <davidkunheeyang@gmail.com>
2024-10-15 15:15:04 -0700

    added flask routes for querying for docs and adding text for embedding and storage

--- backend/flask/app.py
+++ backend/flask/app.py
@@ -29,3 +29,37 @@ def fetch_documents():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+@app.route('/documents', methods=['GET'])
+def fetch_documents():
+    search_string = request.args.get('search')
+    
+    if not search_string:
+        return jsonify({"error": "Missing search parameter"}), 400
+    
+    relevant_documents = list(query(search_string,4))
+    
+    return jsonify({"documents": relevant_documents}), 200
+
+@app.route('/documents', methods=["POST"])
+def process_text():
+    data = request.get_json()
+
+    if not data or 'corpus' not in data:
+        return jsonify({"error": "Missing corpus in request body"}), 400
+
+    userId = data['userId']
+    courseId = data['courseId']
+    title = data['title']
+    corpus = data['corpus']
+
+    chunks = chunk_document_by_sentences(corpus)
+    doc_ids = embed_and_store_docs(userId, courseId, title, chunks)
+
+    return jsonify({
+        "message": "Document successfully chunked, embedded and stored",
+        "documentIds": doc_ids
+    }), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)

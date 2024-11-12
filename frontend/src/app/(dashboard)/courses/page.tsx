88eec91451113860c601fb3636220fc3bb9802c6
loosh <notloosh@gmail.com>
2024-11-11 18:27:57 -0800

    Add redirects for invalid course ids and for blank courses route

--- frontend/src/app/(dashboard)/courses/page.tsx
+++ frontend/src/app/(dashboard)/courses/page.tsx
@@ -1,8 +1,26 @@
-import Editor from '@/components/editor/Editor';
 import { SidebarTrigger } from '@/components/ui/sidebar';
 import { FolderClosed } from 'lucide-react';
+import { auth } from '@clerk/nextjs/server';
+import Course from '@/lib/models/Course';
+import { redirect } from 'next/navigation';
 
-export default function Page() {
+async function getUsersFirstCourse(userId: string): Promise<string | null> {
+  const courses = await Course.find({ userId }).lean();
+
+  if (courses.length === 0) return null;
+
+  return courses[0]._id.toString();
+}
+
+export default async function Page() {
+  const { userId } = auth();
+
+  if (!userId) return redirect('/');
+
+  const firstCourseId = await getUsersFirstCourse(userId);
+
+  if (firstCourseId) return redirect(`/courses/${firstCourseId}`);
+
   return (
     <div className='grid grid-rows-[auto_1fr] h-screen'>
       <div className='sticky top-0 z-10 flex flex-row justify-between p-2 border-b bg-background'>

bf9baec50a34a649aa54f70815566ef7a0d79ba0
d5vis <davisjbanks@gmail.com>
2024-11-13 13:41:35 -0800

    add mini icon to collapsed sidebar

--- frontend/src/components/sidebar/app-sidebar.tsx
+++ frontend/src/components/sidebar/app-sidebar.tsx
@@ -6,17 +6,29 @@ import Image from 'next/image';
 import { CourseContentList } from '@/components/sidebar/course-content-list';
 import { NavUser } from '@/components/sidebar/nav-user';
 import { CourseSwitcher } from '@/components/sidebar/course-switcher';
-import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarRail } from '@/components/ui/sidebar';
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarRail,
+  useSidebar,
+} from '@/components/ui/sidebar';
 
 import { useCoursesContext } from '@/lib/hooks/useCourseContext';
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   const { courses } = useCoursesContext();
+  const { state } = useSidebar();
 
   return (
     <Sidebar collapsible='icon' {...props}>
       <SidebarHeader className='flex'>
-        <Image src='/coursepilot-wide-logo.png' className='p-1' alt='Logo' height={36} width={140} />
+        {state === 'expanded' ? (
+          <Image src='/coursepilot-wide-logo.png' className='p-1' alt='Logo' height={36} width={140} />
+        ) : (
+          <Image src='/favicon.svg' className='p-1' alt='Logo' height={32} width={32} />
+        )}
         <CourseSwitcher />
       </SidebarHeader>
       <SidebarContent>

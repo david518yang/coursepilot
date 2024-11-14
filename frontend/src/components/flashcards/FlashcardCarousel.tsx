35c3c5b88cc40250806a8504a5bba0f118643290
d5vis <davisjbanks@gmail.com>
2024-11-13 16:07:21 -0800

    add flashcard shuffling

--- frontend/src/components/flashcards/FlashcardCarousel.tsx
+++ frontend/src/components/flashcards/FlashcardCarousel.tsx
@@ -9,10 +9,21 @@ import {
 } from '@/components/ui/carousel';
 import { IFlashcard } from '@/lib/models/Flashcard';
 import Flashcard from './Flashcard';
+import { Button } from '../ui/button';
+import { Shuffle } from 'lucide-react';
 
 const FlashcardCarousel = ({ flashcards }: { flashcards: IFlashcard[] }) => {
   const [api, setApi] = useState<CarouselApi>();
   const [index, setIndex] = useState(0);
+  const [shuffledFlashcards, setShuffledFlashcards] = useState(flashcards);
+
+  const shuffleFlashcards = () => {
+    const shuffledFlashcards = [...flashcards].sort(() => Math.random() - 0.5);
+    setShuffledFlashcards(shuffledFlashcards);
+    if (api) {
+      api.scrollTo(0);
+    }
+  };
 
   useEffect(() => {
     if (!api) return;
@@ -26,9 +37,13 @@ const FlashcardCarousel = ({ flashcards }: { flashcards: IFlashcard[] }) => {
 
   return (
     <div className='w-3/4 flex flex-col items-center gap-4'>
+      <Button variant='outline' className='bg-background' onClick={shuffleFlashcards}>
+        <Shuffle className='pr-2' />
+        Shuffle
+      </Button>
       <Carousel setApi={setApi} className='w-full max-w-[512px]'>
         <CarouselContent>
-          {flashcards.map((flashcard, index) => (
+          {shuffledFlashcards.map((flashcard, index) => (
             <CarouselItem key={index}>
               <Flashcard flashcard={flashcard} />
             </CarouselItem>
@@ -38,7 +53,7 @@ const FlashcardCarousel = ({ flashcards }: { flashcards: IFlashcard[] }) => {
         <CarouselNext />
       </Carousel>
       <div>
-        Flashcard {index + 1} of {flashcards.length}
+        Flashcard {index + 1} of {shuffledFlashcards.length}
       </div>
     </div>
   );
